c827de78d956b4a2a327c61131678a3ae314172f
caffonsoabacus <caffonso@abacushealth.com>
2024-10-10 13:20:25 -0400

    Update mgr.js
    formatted this file correctly

--- mgr.js
+++ mgr.js
@@ -1,179 +1,126 @@
-define(['managerAPI',
-		'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe@1.*/datapipe.min.js'], function(Manager){
+define(['managerAPI', 'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe@1.*/datapipe.min.js'], function(Manager) {
 
-
-	//You can use the commented-out code to get parameters from the URL.
-	//const queryString = window.location.search;
-    //const urlParams = new URLSearchParams(queryString);
-    //const pt = urlParams.get('pt');
-
-	var API    = new Manager();
-	//const subid = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
-	init_data_pipe(API, 'MJGly1jNf9pUnhFIS5rOuqAktUzyfIZXVb81A3rsrD5S0Tr19GKFrggwp96cLID2kUn4OU',  {file_type:'csv'});	
+    let API = new Manager();
 
     API.setName('mgr');
     API.addSettings('skip',true);
+	init_data_pipe(API, 'SJkEpXW56fCh',  {file_type:'csv'});	
 
-    //Randomly select which of two sets of category labels to use.
-    let raceSet = API.shuffle(['a','b'])[0];
-    let blackLabels = [];
-    let whiteLabels = [];
-
-    if (raceSet == 'a') {
-        blackLabels.push('African Americans');
-        whiteLabels.push('European Americans');
-    } else {
-        blackLabels.push('Black people');
-        whiteLabels.push('White people');
-    }
-
-    API.addGlobal({
-        raceiat:{},
-        //YBYB: change when copying back to the correct folder
-        baseURL: './images/',
-        raceSet:raceSet,
-        blackLabels:blackLabels,
-        whiteLabels:whiteLabels,
-        //Select randomly what attribute words to see. 
-        //Based on Axt, Feng, & Bar-Anan (2021).
-        posWords : API.shuffle([
-            'Love', 'Cheer', 'Friend', 'Pleasure',
-            'Adore', 'Cheerful', 'Friendship', 'Joyful', 
-            'Smiling','Cherish', 'Excellent', 'Glad', 
-            'Joyous', 'Spectacular', 'Appealing', 'Delight', 
-            'Excitement', 'Laughing', 'Attractive','Delightful', 
-            'Fabulous', 'Glorious', 'Pleasing', 'Beautiful', 
-            'Fantastic', 'Happy', 'Lovely', 'Terrific', 
-            'Celebrate', 'Enjoy', 'Magnificent', 'Triumph'
-        ]), 
-        negWords : API.shuffle([
-            'Abuse', 'Grief', 'Poison', 'Sadness', 
-            'Pain', 'Despise', 'Failure', 'Nasty', 
-            'Angry', 'Detest', 'Horrible', 'Negative', 
-            'Ugly', 'Dirty', 'Gross', 'Evil', 
-            'Rotten','Annoy', 'Disaster', 'Horrific',  
-            'Scorn', 'Awful', 'Disgust', 'Hate', 
-            'Humiliate', 'Selfish', 'Tragic', 'Bothersome', 
-            'Hatred', 'Hurtful', 'Sickening', 'Yucky'
-        ])
-    });
 
     API.addTasksSet({
-        instructions: [{
-            type: 'message',
-            buttonText: 'Continue'
-        }],
-
-        intro: [{
-            inherit: 'instructions',
-            name: 'intro',
-            templateUrl: 'intro.jst',
-            title: 'Intro',
-            header: 'Welcome'
-        }],
-
-        raceiat_instructions: [{
-            inherit: 'instructions',
-            name: 'raceiat_instructions',
-            templateUrl: 'raceiat_instructions.jst',
-            title: 'IAT Instructions',
-            header: 'Implicit Association Test'
-        }],
-
-        explicits: [{
+        choose: [{
             type: 'quest',
-            name: 'explicits',
-            scriptUrl: 'explicits.js'
+            name: 'choose',
+            scriptUrl: 'choose.js'
         }],
 
-        raceiat: [{
+        age: [{
             type: 'time',
-            name: 'raceiat',
-            scriptUrl: 'raceiat.js'
+            name: 'age',
+            scriptUrl: 'age.js'
         }],
-
-        lastpage: [{
-            type: 'message',
-            name: 'lastpage',
-            templateUrl: 'lastpage.jst',
-            title: 'End',
-            //Uncomment the following if you want to end the study here.
-            //last:true, 
-            header: 'You have completed the study'
-        }], 
-        
-        //Use if you want to redirect the participants elsewhere at the end of the study
-        redirect:
-        [{ 
-			//Replace with any URL you need to put at the end of your study, or just remove this task from the sequence below
-            type:'redirect', name:'redirecting', url: 'https://www.google.com/search' 
+        arab: [{
+            type: 'time',
+            name: 'arab',
+            scriptUrl: 'arab.js'
         }],
-		
-		//This task waits until the data are sent to the server.
-        uploading: uploading_task({header: 'just a moment', body:'Please wait, sending data... '})
+        asian: [{
+            type: 'time',
+            name: 'asian',
+            scriptUrl: 'asian.js'
+        }],
+        disability: [{
+            type: 'time',
+            name: 'disability',
+            scriptUrl: 'disability.js'
+        }],
+        nativee: [{
+            type: 'time',
+            name: 'nativee',
+            scriptUrl: 'native.js'
+        }],
+        genderscience: [{
+            type: 'time',
+            name: 'genderscience',
+            scriptUrl: 'genderscience.js'
+        }],
+        sexuality: [{
+            type: 'time',
+            name: 'sexuality',
+            scriptUrl: 'sexuality.js'
+        }],
+        weight: [{
+            type: 'time',
+            name: 'weight',
+            scriptUrl: 'weight.js'
+        }],
+	hispanic: [{
+            type: 'time',
+            name: 'hispanic',
+            scriptUrl: 'hispanic.js'
+        }],
+        race: [{
+            type: 'time',
+            name: 'race',
+            scriptUrl: 'race.js'
+        }]
     });
 
     API.addSequence([
-        { type: 'isTouch' }, //Use Minno's internal touch detection mechanism. 
-        
-        { type: 'post', path: ['$isTouch', 'raceSet', 'blackLabels', 'whiteLabels'] },
-
-        // apply touch only styles
-        {
-            mixer:'branch',
-            conditions: {compare:'global.$isTouch', to: true},
-            data: [
-                {
-                    type: 'injectStyle',
-                    css: [
-                        '* {color:red}',
-                        '[piq-page] {background-color: #fff; border: 1px solid transparent; border-radius: 4px; box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05); margin-bottom: 20px; border-color: #bce8f1;}',
-                        '[piq-page] > ol {margin: 15px;}',
-                        '[piq-page] > .btn-group {margin: 0px 15px 15px 15px;}',
-                        '.container {padding:5px;}',
-                        '[pi-quest]::before, [pi-quest]::after {content: " ";display: table;}',
-                        '[pi-quest]::after {clear: both;}',
-                        '[pi-quest] h3 { border-bottom: 1px solid transparent; border-top-left-radius: 3px; border-top-right-radius: 3px; padding: 10px 15px; color: inherit; font-size: 2em; margin-bottom: 20px; margin-top: 0;background-color: #d9edf7;border-color: #bce8f1;color: #31708f;}',
-                        '[pi-quest] .form-group > label {font-size:1.2em; font-weight:normal;}',
-
-                        '[pi-quest] .btn-toolbar {margin:15px;float:none !important; text-align:center;position:relative;}',
-                        '[pi-quest] [ng-click="decline($event)"] {position:absolute;right:0;bottom:0}',
-                        '[pi-quest] [ng-click="submit()"] {width:30%;line-height: 1.3333333;border-radius: 6px;}',
-                        // larger screens
-                        '@media (min-width: 480px) {',
-                        ' [pi-quest] [ng-click="submit()"] {width:30%;padding: 10px 16px;font-size: 1.6em;}',
-                        '}',
-                        // phones and smaller screens
-                        '@media (max-width: 480px) {',
-                        ' [pi-quest] [ng-click="submit()"] {padding: 8px 13px;font-size: 1.2em;}',
-                        ' [pi-quest] [ng-click="decline($event)"] {font-size: 0.9em;padding:3px 6px;}',
-                        '}'
-                    ]
-                }
-            ]
-        },
-        
-        
-        {inherit: 'intro'},
-        {
-            mixer:'random',
-            data:[
-                {inherit: 'explicits'},
-
-                // force the instructions to preceed the iat
-                {
-                    mixer: 'wrapper',
-                    data: [
-                        {inherit: 'raceiat_instructions'},
-                        {inherit: 'raceiat'}
-                    ]
-                }
-            ]
-        },
-
-		{inherit: 'uploading'},
-        {inherit: 'lastpage'},
-        {inherit: 'redirect'}
+	    {
+	        mixer:'repeat',
+	        times:10, 
+	        data :
+	        [
+	            {inherit:'choose'},
+	            {
+	                mixer:'multiBranch', 
+	                branches :
+	                [
+	                    { 
+	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'age'}],
+	                        data:[{inherit:'age'}]
+	                    },
+	                    { 
+	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'arab'}],
+	                        data:[{inherit:'arab'}]
+	                    },
+	                    { 
+	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'asian'}],
+	                        data:[{inherit:'asian'}]
+	                    },
+	                    { 
+	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'disability'}],
+	                        data:[{inherit:'disability'}]
+	                    },
+	                    { 
+	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'genderscience'}],
+	                        data:[{inherit:'genderscience'}]
+	                    },
+	                    { 
+	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'native'}],
+	                        data:[{inherit:'nativee'}]
+	                    },
+	                    { 
+	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'race'}],
+	                        data:[{inherit:'race'}]
+	                    },
+	                    { 
+	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'sexuality'}],
+	                        data:[{inherit:'sexuality'}]
+	                    },
+	                    { 
+	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'hispanic'}],
+	                        data:[{inherit:'hispanic'}]
+	                    },
+	                    { 
+	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'weight'}],
+	                        data:[{inherit:'weight'}]
+	                    }
+	                ]
+	            }
+	       ]
+	    }
     ]);
 
     return API.script;
