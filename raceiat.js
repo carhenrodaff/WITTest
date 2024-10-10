b23b0a2c40493f1427171838a83d2a40788f28b4
caffonsoabacus <caffonso@abacushealth.com>
2024-10-10 11:46:46 -0400

    Update raceiat.js to weightiat.js
    changed this file to reflect weightiat

--- raceiat.js
+++ raceiat.js
@@ -1,88 +1,122 @@
-define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.js'], function(APIConstructor, iatExtension){
-    let API = new APIConstructor();
-    let global = API.getGlobal();
+define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat8.js'], function(APIConstructor, iatExtension){
+	var API = new APIConstructor();
 
+    API.addGlobal({
+        //Based on Axt, Feng, & Bar-Anan (2021).
+        posWords : API.shuffle([
+            'Love', 'Cheer', 'Friend', 'Pleasure',
+            'Adore', 'Cheerful', 'Friendship', 'Joyful', 
+            'Smiling','Cherish', 'Excellent', 'Glad', 
+            'Joyous', 'Spectacular', 'Appealing', 'Delight', 
+            'Excitement', 'Laughing', 'Attractive','Delightful', 
+            'Fabulous', 'Glorious', 'Pleasing', 'Beautiful', 
+            'Fantastic', 'Happy', 'Lovely', 'Terrific', 
+            'Celebrate', 'Enjoy', 'Magnificent', 'Triumph'
+        ]), 
+        negWords : API.shuffle([
+            'Abuse', 'Grief', 'Poison', 'Sadness', 
+            'Pain', 'Despise', 'Failure', 'Nasty', 
+            'Angry', 'Detest', 'Horrible', 'Negative', 
+            'Ugly', 'Dirty', 'Gross', 'Evil', 
+            'Rotten','Annoy', 'Disaster', 'Horrific',  
+            'Scorn', 'Awful', 'Disgust', 'Hate', 
+            'Humiliate', 'Selfish', 'Tragic', 'Bothersome', 
+            'Hatred', 'Hurtful', 'Sickening', 'Yucky'
+        ])
+    });
+
+    var global = API.getGlobal();
+    
     return iatExtension({
         category1 : {
-            name : global.blackLabels, //Will appear in the data.
+            name : 'Fat people', //Will appear in the data.
             title : {
-                media : {word : global.blackLabels}, //Name of the category presented in the task.
+                media : {word : 'Fat people'}, //Name of the category presented in the task.
                 css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
                 height : 4 //Used to position the "Or" in the combined block.
             }, 
             stimulusMedia : [ //Stimuli content as PIP's media objects
-                {image: 'bm1_nc.jpg'},
-                {image: 'bm2_nc.jpg'},
-                {image: 'bm3_nc.jpg'},
-                {image: 'bf1_nc.jpg'},
-                {image: 'bf2_nc.jpg'},                 
-                {image: 'bf3_nc.jpg'}     
-            ],
-            //Stimulus css (style)
-            stimulusCss : {color:'#31940F','font-size':'2.3em'}
+                {image: 'fatman1.jpg'},
+                {image: 'fatman3.jpg'},
+                {image: 'fatman4.jpg'},
+				{image: 'fatman5.jpg'},
+                {image: 'fatman6.jpg'},                 
+				{image: 'fatwoman1.jpg'},
+                {image: 'fatwoman2.jpg'},
+                {image: 'fatwoman3.jpg'},
+                {image: 'fatwoman4.jpg'},
+                {image: 'fatwoman6.jpg'}     
+    	    ],
+    		//Stimulus css (style)
+    		stimulusCss : {color:'#31940F','font-size':'2.3em'}
         },    
-        category2 : {
-            name : global.whiteLabels, //Will appear in the data.
+        category2 :    {
+            name : 'Thin people', //Will appear in the data.
             title : {
-                media : {word : global.whiteLabels}, //Name of the category presented in the task.
+                media : {word : 'Thin people'}, //Name of the category presented in the task.
                 css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
                 height : 4 //Used to position the "Or" in the combined block.
             }, 
             stimulusMedia : [ //Stimuli content as PIP's media objects
-                {image: 'wm1_nc.jpg'},
-                {image: 'wm2_nc.jpg'},
-                {image: 'wm3_nc.jpg'},
-                {image: 'wf1_nc.jpg'},
-                {image: 'wf2_nc.jpg'},
-                {image: 'wf3_nc.jpg'}
+                {image: 'thinman1.jpg'},
+                {image: 'thinman2.jpg'},
+                {image: 'thinman3.jpg'},
+				{image: 'thinman5.jpg'},
+                {image: 'thinman6.jpg'},                 
+				{image: 'thinwoman1.jpg'},
+                {image: 'thinwoman2.jpg'},
+                {image: 'thinwoman3.jpg'},
+				{image: 'thinwoman5.jpg'},
+                {image: 'thinwoman6.jpg'}  
             ],
-            //Stimulus css (style)
-            stimulusCss : {color:'#31940F','font-size':'2.3em'}
+    		//Stimulus css (style)
+    		stimulusCss : {color:'#31940F','font-size':'2.3em'}
         },
-        attribute1 : {
-            name : 'Bad words',
-            title : {
-                media : {word : 'Bad words'},
-                css : {color:'#0000FF','font-size':'1.8em'},
-                height : 4 //Used to position the "Or" in the combined block.
-            },
-            stimulusMedia : [ //Stimuli content as PIP's media objects
-                {word: global.negWords[0]},
-                {word: global.negWords[1]},
-                {word: global.negWords[2]},
-                {word: global.negWords[3]},
-                {word: global.negWords[4]},
-                {word: global.negWords[5]},
-                {word: global.negWords[6]},
-                {word: global.negWords[7]}
-            ],
-            //Stimulus css
-            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
-        },
-        attribute2 : {
-            name : 'Good words',
-            title : {
-                media : {word : 'Good words'},
-                css : {color:'#0000FF','font-size':'1.8em'},
-                height : 4 //Used to position the "Or" in the combined block.
-            },
-            stimulusMedia : [ //Stimuli content as PIP's media objects
-                {word: global.posWords[0]},
-                {word: global.posWords[1]},
-                {word: global.posWords[2]},
-                {word: global.posWords[3]},
-                {word: global.posWords[4]},
-                {word: global.posWords[5]},
-                {word: global.posWords[6]},
-                {word: global.posWords[7]}
-            ],
-            //Stimulus css
-            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
-        },
+		attribute1 :
+		{
+			name : 'Bad words',
+			title : {
+				media : {word : 'Bad words'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: global.negWords[0]},
+				{word: global.negWords[1]},
+				{word: global.negWords[2]},
+				{word: global.negWords[3]},
+				{word: global.negWords[4]},
+				{word: global.negWords[5]},
+				{word: global.negWords[6]},
+				{word: global.negWords[7]}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+		attribute2 :
+		{
+			name : 'Good words',
+			title : {
+				media : {word : 'Good words'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: global.posWords[0]},
+				{word: global.posWords[1]},
+				{word: global.posWords[2]},
+				{word: global.posWords[3]},
+				{word: global.posWords[4]},
+				{word: global.posWords[5]},
+				{word: global.posWords[6]},
+				{word: global.posWords[7]}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
         base_url : {//Where are your images at?
-            image : global.baseURL
+            image : 'https://baranan.github.io/minno-tasks/studies/datapipe.iat.examples/demo.iats/weight.images/'
         },
-        isTouch : global.$isTouch
+		isTouch : global.isTouch
     });
 });
-
