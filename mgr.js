0f92e783403562cd397d3335265034f41ef02b1b
caffonsoabacus <caffonso@abacushealth.com>
2024-10-10 13:34:47 -0400

    Update mgr.js

--- mgr.js
+++ mgr.js
@@ -1,127 +1,122 @@
-define(['managerAPI', 'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe@1.*/datapipe.min.js'], function(Manager) {
+define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat8.js'], function(APIConstructor, iatExtension){
+	var API = new APIConstructor();
 
-    let API = new Manager();
-
-    API.setName('mgr');
-    API.addSettings('skip',true);
-	init_data_pipe(API, 'SJkEpXW56fCh',  {file_type:'csv'});	
-
-
-    API.addTasksSet({
-        choose: [{
-            type: 'quest',
-            name: 'choose',
-            scriptUrl: 'choose.js'
-        }],
-
-        age: [{
-            type: 'time',
-            name: 'age',
-            scriptUrl: 'age.js'
-        }],
-        arab: [{
-            type: 'time',
-            name: 'arab',
-            scriptUrl: 'arab.js'
-        }],
-        asian: [{
-            type: 'time',
-            name: 'asian',
-            scriptUrl: 'asian.js'
-        }],
-        disability: [{
-            type: 'time',
-            name: 'disability',
-            scriptUrl: 'disability.js'
-        }],
-        nativee: [{
-            type: 'time',
-            name: 'nativee',
-            scriptUrl: 'native.js'
-        }],
-        genderscience: [{
-            type: 'time',
-            name: 'genderscience',
-            scriptUrl: 'genderscience.js'
-        }],
-        sexuality: [{
-            type: 'time',
-            name: 'sexuality',
-            scriptUrl: 'sexuality.js'
-        }],
-        weight: [{
-            type: 'time',
-            name: 'weight',
-            scriptUrl: 'weight.js'
-        }],
-	hispanic: [{
-            type: 'time',
-            name: 'hispanic',
-            scriptUrl: 'hispanic.js'
-        }],
-        race: [{
-            type: 'time',
-            name: 'race',
-            scriptUrl: 'race.js'
-        }]
+    API.addGlobal({
+        //Based on Axt, Feng, & Bar-Anan (2021).
+        posWords : API.shuffle([
+            'Love', 'Cheer', 'Friend', 'Pleasure',
+            'Adore', 'Cheerful', 'Friendship', 'Joyful', 
+            'Smiling','Cherish', 'Excellent', 'Glad', 
+            'Joyous', 'Spectacular', 'Appealing', 'Delight', 
+            'Excitement', 'Laughing', 'Attractive','Delightful', 
+            'Fabulous', 'Glorious', 'Pleasing', 'Beautiful', 
+            'Fantastic', 'Happy', 'Lovely', 'Terrific', 
+            'Celebrate', 'Enjoy', 'Magnificent', 'Triumph'
+        ]), 
+        negWords : API.shuffle([
+            'Abuse', 'Grief', 'Poison', 'Sadness', 
+            'Pain', 'Despise', 'Failure', 'Nasty', 
+            'Angry', 'Detest', 'Horrible', 'Negative', 
+            'Ugly', 'Dirty', 'Gross', 'Evil', 
+            'Rotten','Annoy', 'Disaster', 'Horrific',  
+            'Scorn', 'Awful', 'Disgust', 'Hate', 
+            'Humiliate', 'Selfish', 'Tragic', 'Bothersome', 
+            'Hatred', 'Hurtful', 'Sickening', 'Yucky'
+        ])
     });
 
-    API.addSequence([
-	    {
-	        mixer:'repeat',
-	        times:10, 
-	        data :
-	        [
-	            {inherit:'choose'},
-	            {
-	                mixer:'multiBranch', 
-	                branches :
-	                [
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'age'}],
-	                        data:[{inherit:'age'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'arab'}],
-	                        data:[{inherit:'arab'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'asian'}],
-	                        data:[{inherit:'asian'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'disability'}],
-	                        data:[{inherit:'disability'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'genderscience'}],
-	                        data:[{inherit:'genderscience'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'native'}],
-	                        data:[{inherit:'nativee'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'race'}],
-	                        data:[{inherit:'race'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'sexuality'}],
-	                        data:[{inherit:'sexuality'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'hispanic'}],
-	                        data:[{inherit:'hispanic'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'weight'}],
-	                        data:[{inherit:'weight'}]
-	                    }
-	                ]
-	            }
-	       ]
-	    }
-    ]);
-
-    return API.script;
+    var global = API.getGlobal();
+    
+    return iatExtension({
+        category1 : {
+            name : 'Fat people', //Will appear in the data.
+            title : {
+                media : {word : 'Fat people'}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'fatman1.jpg'},
+                {image: 'fatman3.jpg'},
+                {image: 'fatman4.jpg'},
+				{image: 'fatman5.jpg'},
+                {image: 'fatman6.jpg'},                 
+				{image: 'fatwoman1.jpg'},
+                {image: 'fatwoman2.jpg'},
+                {image: 'fatwoman3.jpg'},
+                {image: 'fatwoman4.jpg'},
+                {image: 'fatwoman6.jpg'}     
+    	    ],
+    		//Stimulus css (style)
+    		stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },    
+        category2 :    {
+            name : 'Thin people', //Will appear in the data.
+            title : {
+                media : {word : 'Thin people'}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'thinman1.jpg'},
+                {image: 'thinman2.jpg'},
+                {image: 'thinman3.jpg'},
+				{image: 'thinman5.jpg'},
+                {image: 'thinman6.jpg'},                 
+				{image: 'thinwoman1.jpg'},
+                {image: 'thinwoman2.jpg'},
+                {image: 'thinwoman3.jpg'},
+				{image: 'thinwoman5.jpg'},
+                {image: 'thinwoman6.jpg'}  
+            ],
+    		//Stimulus css (style)
+    		stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },
+		attribute1 :
+		{
+			name : 'Bad words',
+			title : {
+				media : {word : 'Bad words'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: global.negWords[0]},
+				{word: global.negWords[1]},
+				{word: global.negWords[2]},
+				{word: global.negWords[3]},
+				{word: global.negWords[4]},
+				{word: global.negWords[5]},
+				{word: global.negWords[6]},
+				{word: global.negWords[7]}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+		attribute2 :
+		{
+			name : 'Good words',
+			title : {
+				media : {word : 'Good words'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: global.posWords[0]},
+				{word: global.posWords[1]},
+				{word: global.posWords[2]},
+				{word: global.posWords[3]},
+				{word: global.posWords[4]},
+				{word: global.posWords[5]},
+				{word: global.posWords[6]},
+				{word: global.posWords[7]}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+        base_url : {//Where are your images at?
+            image : 'https://caffonsoabacus.github.io/WITTest/images'
+        },
+		isTouch : global.isTouch
+    });
 });
