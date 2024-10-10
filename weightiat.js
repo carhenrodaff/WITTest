37cb3030a55be9e0f55eb68d17d0a12f1848e031
caffonsoabacus <caffonso@abacushealth.com>
2024-10-10 13:11:25 -0400

    Update weightiat.js

--- weightiat.js
+++ weightiat.js
@@ -115,7 +115,7 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat8.j
 			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
 		},
         base_url : {//Where are your images at?
-            image : 'https://caffonsoabacus.github.io/WITTest/images'
+            image : 'https://baranan.github.io/minno-tasks/studies/datapipe.iat.examples/demo.iats/weight.images/'
         },
 		isTouch : global.isTouch
     });
