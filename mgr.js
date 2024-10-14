7312ba8504714ff755264ad90bc97ea5753e41ce
Carlos Affonso <carlos.affonso@staticsoftware.net>
2024-10-14 17:19:22 -0400

    correct key

--- mgr.js
+++ mgr.js
@@ -9,7 +9,7 @@ define([
 
   var API = new Manager();
   //const subid = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
-  init_data_pipe(API, "q0EUCMxhz8yo", { file_type: "csv" });
+  init_data_pipe(API, "OVK2ZYrjUGPs", { file_type: "csv" });
 
   API.setName("mgr");
   API.addSettings("skip", true);
